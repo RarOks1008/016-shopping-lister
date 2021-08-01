8396960fb7d850450b03ffcea6fa2dd04a11602c
Nikola Nedeljkovic <nikola.nini@gmail.com>
2021-08-01 11:55:14 +0200

    Adding the image

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,13 +24,20 @@ export class HomePage {
   }
 
   public addToList() {
-    // if (!this.title) return;
-    var last_id = 0;
+    if (!this.title) return;
+    var last_id = 0,
+        image = '';
     this.products.products.forEach(pr => {
       const current_id = parseInt(pr.id.substring(1));
       if (current_id > last_id) last_id = current_id;
+      if (this.title == pr.title) image = pr.imageURL;
     });
-    console.log(last_id);
+    this.products.products.push({
+      id: 'a' + last_id++,
+      title: this.title,
+      imageURL: image
+    });
+    this.title = '';
   }
 
   ngOnInit() {
